07ecf5cb8aa4fac7817dcadb43a466866ca2c7d7
badger <badger@badger.local>
2022-05-14 12:20:47 +0300

    Set PGPASSWORD to postgrespassword

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      Test travis!
       <Link to="/">Go back home</Link>
     </div>
   );
